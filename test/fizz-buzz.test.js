904bfa375f4348f91265b54ed0baa1f81f499241
Sueleyman Bilgi <sueleyman.bilgi@sap.com>
2021-11-11 15:51:29 +0100

    fix typo

--- test/fizz-buzz.test.js
+++ test/fizz-buzz.test.js
@@ -3,7 +3,7 @@ const FizzBuzz = require('../lib/fizz-buzz')
 
 describe('fizz-buzz', function () {
   it('example failing test', () => {
-    const fizzBuzz = new FizzbBuzz()
+    const fizzBuzz = new FizzBuzz()
     const result = fizzBuzz.evaluate(1)
     // TODO: add your assertions here
     // @see https://nodejs.org/dist/latest-v16.x/docs/api/assert.html#assert
